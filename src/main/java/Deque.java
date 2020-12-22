6c313bc8bd9a982bcd718bf8b26450358eba997c
eonegin <3he@mail.ru>
2020-12-22 10:59:10 +0300

    Deque with QueueThroughStack1 into //v3

--- src/main/java/Deque.java
+++ src/main/java/Deque.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Deque<T> {
 
-    QueueThroughStack<T> deque;
+    QueueThroughStack1<T> deque;
 
     public Deque() {
-        deque = new QueueThroughStack<>();
+        deque = new QueueThroughStack1<>();
     }
 
     public void addFront(T item) {
-        QueueThroughStack<T> deque2 = new QueueThroughStack<T>();
+        QueueThroughStack1<T> deque2 = new QueueThroughStack1<T>();
         deque2.enqueue(item);
         while (deque.stack1.size() > 0) {
             deque2.stack1.push(deque.dequeue());
@@ -32,5 +32,44 @@ public class Deque<T> {
 
     public int size() {
         return deque.size();
+    }
+}
+
+class QueueThroughStack1<T> {
+
+    Stack<T> stack1;
+    Stack<T> stack2;
+
+    public QueueThroughStack1() {
+        // инициализация внутреннего хранилища очереди
+        stack1 = new Stack<T>();
+
+    }
+
+    //Добавление элемента в очередь
+    public void enqueue(Object item) {
+        stack1.push((T) item);
+    }
+
+    //Удаление элемента из очереди
+    public T dequeue() {
+        stack2 = new Stack<T>();
+        while (stack1.size() > 1) {
+            if (stack2.size() == 0) {
+                stack2.push(stack1.pop());
+            } else if (stack2.size() > 0) {
+                T k = stack2.pop();
+                stack2.push(stack1.pop());
+                stack2.push(k);
+            }
+        }
+        T j = stack1.pop();
+        stack1 = stack2;
+        return j;
+    }
+
+    public int size() {
+        // размер очереди
+        return stack1.size();
     }
 }
