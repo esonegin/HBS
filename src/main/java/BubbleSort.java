b4ee60887445bde7e340b855017e5e0256e14014
”Evgeniy <onegin279@gmail.com>
2021-08-04 11:18:04 +0300

    Пузырьковая сортировка

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,30 +1,15 @@
 public class BubbleSort {
 
     public static int[] bubbleSort(int[] array) {
-        //Если массив содержит один элемент - возвращаем массив
-        if(array.length == 1){
-            return array;
-        }
-        //Переменная для хранения промежуточного минимума
-        int min;
-        //Переменная для фиксации итераций
-        int j = 0;
-        //Переменная для отсекания индекса отсортированного числа
-        int k = 0;
-
-        for (int i = array.length - 1; k <= array.length; i--) {
-            if(j == array.length - 1) {
-                j = 0;
-                k++;
-                i = array.length - 1;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    int k = array[i];
+                    array[i] = array[j];
+                    array[j] = k;
+                }
             }
 
-            if (array[i] < array[i - 1]) {
-                min = array[i];
-                array[i] = array[i - 1];
-                array[i - 1] = min;
-            }
-            j++;
         }
         return array;
     }
