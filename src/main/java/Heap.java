fa0ef2f5cee26cbd2c65af3485113990f9e18734
esonegin <Dslhf456>
2022-04-13 10:56:17 +0300

    v1

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Heap {
     public int[] HeapArray; // хранит неотрицательные числа-ключи
@@ -15,85 +15,134 @@ class Heap {
         }
         tree_size = tree_size - 1;
         HeapArray = new int[tree_size];
-
-        for (int i = 0; i < tree_size; i++)
-            //HeapArray[i] = 0;
-            HeapArray[0] = getMaxOnArray(a);
-            delMax(a, getMaxOnArray(a));
-
-        for (int j : a) {
-            Add(j);
+        Arrays.fill(HeapArray, -1);
+        for (int i = 0; i < HeapArray.length && i < a.length; i++) {
+            Add(a[i]);
         }
+        List<Integer> prom = new ArrayList<>();
+        for (int j : HeapArray) {
+            if (j != -1) {
+                prom.add(j);
+            }
+        }
+        int[] result = new int[prom.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = prom.get(i);
+        }
+        HeapArray = result;
         // создаём массив кучи HeapArray из заданного
         // размер массива выбираем на основе глубины depth
         // ...
     }
 
-    public int GetMax() {
-        int max = HeapArray[0];
-        int index = 0;
-        if (HeapArray[0] != 0) {
-            for (int i = HeapArray.length - 1; i != 0; i--) {
-                if (HeapArray[i] != 0) {
-                    index = i;
-                    break;
-                }
-            }
-            HeapArray[0] = HeapArray[index];
-            HeapArray[index] = 0;
-            downNode();
-            return max;
-        }
-        // вернуть значение корня и перестроить кучу
-        return -1; // если куча пуста
-    }
 
     public boolean Add(int key) {
-        if (HeapArray[HeapArray.length - 1] == 0 && key != 0) {
-            HeapArray[HeapArray.length - 1] = key;
-            upNode();
-            return true;
-        }
+        int parrent = 0;
         // добавляем новый элемент key в кучу и перестраиваем её
+        for (int i = 0; i < HeapArray.length; i++) {
+            if (i != 0 && i % 2 == 0) {
+                parrent = HeapArray[i / 2 - 1];
+            } else if (i != 0) {
+                parrent = HeapArray[i / 2];
+            }
+            //Если свободен корень, кладем значение в корень
+            if (i == 0 && HeapArray[0] == -1) {
+                HeapArray[0] = key;
+                break;
+            }
+            //Если индекс свободен и родитель больше значения, кладем в индекс
+            if (HeapArray[i] == -1 && parrent >= key) {
+                HeapArray[i] = key;
+                break;
+            }
+            //Если индекс свободен и родитель меньше значения, ищем подходящий индекс
+            if (HeapArray[i] == -1 && parrent <= key) {
+                int[] rotate = UpNode(i, key);
+
+                int p = HeapArray[rotate[0]];
+                HeapArray[rotate[0]] = key;
+                HeapArray[i] = HeapArray[rotate[1]];
+                HeapArray[rotate[1]] = p;
+                break;
+            }
+
+
+        }
         return false; // если куча вся заполнена
     }
 
-    public void upNode() {
-        for (int i = HeapArray.length - 1; i != 0; i--) {
-            if (HeapArray[i - 1] < HeapArray[i]) {
-                int r = HeapArray[i];
-                HeapArray[i] = HeapArray[i - 1];
-                HeapArray[i - 1] = r;
-            }
+
+    public int GetMax() {
+        int max = HeapArray[0];
+        if (HeapArray[0] == -1) {
+            return -1; // если куча пуста
+        }
+        HeapArray[0] = HeapArray[HeapArray.length - 1];
+        DownNode(0, HeapArray[0]);
+
+        return max;
+    }
+
+    //Опускаем, пока значение не будет больше потомка
+    public void DownNode(int index, int key) {
+        int[] largerchild = FindLargerChild(index);
+        if (index == 0) {
+            int[] result = new int[HeapArray.length - 1];
+            System.arraycopy(HeapArray, 0, result, 0, result.length);
+            HeapArray = result;
+        }
+        HeapArray[index] = largerchild[1];
+        HeapArray[largerchild[0]] = key;
+        while (FindLargerChild(largerchild[0]) != null) {
+            DownNode(largerchild[0], key);
         }
     }
 
-    public void downNode() {
-        for (int i = 0; i < HeapArray.length; i++) {
-            if (i + 1 < HeapArray.length && HeapArray[i] < HeapArray[i + 1]) {
-                int r = HeapArray[i + 1];
-                HeapArray[i + 1] = HeapArray[i];
-                HeapArray[i] = r;
+    public int[] UpNode(int index, int key) {
+        int parrent = 0;
+        int[] result = new int[2];
+        result[0] = index;
+        while (HeapArray[parrent] < key) {
+            if (result[0] != 0 && result[0] % 2 == 0) {
+                parrent = Math.round(result[0] / 2 - 1);
+            } else if (result[0] != 0) {
+                parrent = Math.round(result[0] / 2);
+            }
+            result[1] = result[0];
+            result[0] = parrent;
+            if (result[0] == 0) {
+                break;
             }
         }
+        return result;
     }
 
-    public static int getMaxOnArray(int[] inputArray) {
-        int maxValue = inputArray[0];
-        for (int i = 1; i < inputArray.length; i++) {
-            if (inputArray[i] > maxValue) {
-                maxValue = inputArray[i];
+    public int[] FindLargerChild(int index) {
+        if (index * 2 + 1 >= HeapArray.length - 1) {
+            return null;
+        }
+        int[] largerchild = new int[2];
+        if (index == 0) {
+            largerchild[1] = Math.max(HeapArray[1], HeapArray[2]);
+            if (HeapArray[index] > largerchild[1]) {
+                return null;
+            }
+            if (HeapArray[1] >= HeapArray[2]) {
+                largerchild[0] = 1;
+            } else {
+                largerchild[0] = 2;
+            }
+        } else {
+            largerchild[1] = Math.max(HeapArray[index * 2 + 1], HeapArray[index * 2 + 2]);
+            if (HeapArray[index] > largerchild[1]) {
+                return null;
+            }
+            if (HeapArray[index * 2 + 1] > HeapArray[index * 2 + 2]) {
+                largerchild[0] = index * 2 + 1;
+            } else {
+                largerchild[0] = index * 2 + 2;
             }
         }
-        return maxValue;
+        return largerchild;
     }
-
-    public static void delMax(int[] inputArray, int valfordel) {
-        for (int i = 0; i < inputArray.length; i++) {
-            if (inputArray[i] == valfordel) {
-                inputArray[i] = 0;
-            }
-        }
-    }
-
 }
--- src/test/java/HeapTest.java
+++ src/test/java/HeapTest.java
@@ -25,226 +25,136 @@ public class HeapTest {
         Heap heap = new Heap();
         heap.MakeHeap(array, 2);
 
+        Assert.assertThat(heap.HeapArray.length, is(7));
         Assert.assertThat(heap.HeapArray[0], is(14));
-        Assert.assertThat(heap.HeapArray[1], is(12));
-        Assert.assertThat(heap.HeapArray[2], is(10));
-        Assert.assertThat(heap.HeapArray[3], is(8));
-        Assert.assertThat(heap.HeapArray[4], is(6));
-        Assert.assertThat(heap.HeapArray[5], is(4));
-        Assert.assertThat(heap.HeapArray[6], is(2));
-    }
-
-    @Test
-    public void errorAddTest() {
-
-        int[] array = {12, 8, 4};
-        Heap heap = new Heap();
-        heap.MakeHeap(array, 2);
-
-        Assert.assertThat(heap.HeapArray[0], is(12));
         Assert.assertThat(heap.HeapArray[1], is(8));
-        Assert.assertThat(heap.HeapArray[2], is(4));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-    }
-
-    @Test
-    public void randAddTest() {
-
-        int[] array = {8, 4, 12};
-        Heap heap = new Heap();
-        heap.MakeHeap(array, 2);
-
-        Assert.assertThat(heap.HeapArray[0], is(12));
-        Assert.assertThat(heap.HeapArray[1], is(8));
-        Assert.assertThat(heap.HeapArray[2], is(4));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-    }
-
-    @Test
-    public void overDepthAddTest() {
-
-        int[] array = {8, 4, 12, 43, 2};
-        Heap heap = new Heap();
-        heap.MakeHeap(array, 1);
-
-        Assert.assertThat(heap.HeapArray[0], is(43));
-        Assert.assertThat(heap.HeapArray[1], is(8));
-        Assert.assertThat(heap.HeapArray[2], is(4));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-    }
-
-    @Test
-    public void over2DepthAddTest() {
-
-        int[] array = {8, 4, 12, 43, 2};
-        Heap heap = new Heap();
-        heap.MakeHeap(array, 2);
-
-        Assert.assertThat(heap.HeapArray[0], is(43));
-        Assert.assertThat(heap.HeapArray[1], is(12));
-        Assert.assertThat(heap.HeapArray[2], is(8));
-        Assert.assertThat(heap.HeapArray[3], is(4));
+        Assert.assertThat(heap.HeapArray[2], is(12));
+        Assert.assertThat(heap.HeapArray[3], is(6));
         Assert.assertThat(heap.HeapArray[4], is(2));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-    }
-
-    @Test
-    public void addOverTest() {
-        
-        Heap heap = new Heap();
-        heap.HeapArray = new int[7];
-        //heap.MakeHeap(array, 2);
-
-        Assert.assertThat(heap.Add(12) == true, is(true));
-        Assert.assertThat(heap.Add(8) == true, is(true));
-        Assert.assertThat(heap.Add(4) == true, is(true));
-        Assert.assertThat(heap.Add(6) == true, is(true));
-        Assert.assertThat(heap.Add(2) == true, is(true));
-        Assert.assertThat(heap.Add(14) == true, is(true));
-        Assert.assertThat(heap.Add(10) == true, is(true));
-        
-        for(int i = 0; i < 1000; i++){
-            Assert.assertThat(heap.Add(i) == false, is(true));
-        }
-
-        Assert.assertThat(heap.HeapArray[0], is(14));
-        Assert.assertThat(heap.HeapArray[1], is(12));
-        Assert.assertThat(heap.HeapArray[2], is(10));
-        Assert.assertThat(heap.HeapArray[3], is(8));
-        Assert.assertThat(heap.HeapArray[4], is(6));
         Assert.assertThat(heap.HeapArray[5], is(4));
-        Assert.assertThat(heap.HeapArray[6], is(2));
+        Assert.assertThat(heap.HeapArray[6], is(10));
     }
 
     @Test
-    public void delMaxDefTEst() {
+    public void defaultOverAddTest() {
+        int[] array = {12, 8, 4, 6, 2, 14, 10, 11};
+        Heap heap = new Heap();
+        heap.MakeHeap(array, 2);
+        Assert.assertThat(heap.HeapArray.length, is(7));
+        Assert.assertThat(heap.HeapArray[0], is(14));
+        Assert.assertThat(heap.HeapArray[1], is(8));
+        Assert.assertThat(heap.HeapArray[2], is(12));
+        Assert.assertThat(heap.HeapArray[3], is(6));
+        Assert.assertThat(heap.HeapArray[4], is(2));
+        Assert.assertThat(heap.HeapArray[5], is(4));
+        Assert.assertThat(heap.HeapArray[6], is(10));
+        Assert.assertThat(heap.Add(11), is(false));
+        Assert.assertThat(heap.HeapArray.length, is(7));
+    }
 
+    @Test
+    public void everyStepUpAddTest() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        Heap heap = new Heap();
+        heap.MakeHeap(array, 2);
+        Assert.assertThat(heap.HeapArray[0], is(7));
+        Assert.assertThat(heap.HeapArray[1], is(4));
+        Assert.assertThat(heap.HeapArray[2], is(6));
+        Assert.assertThat(heap.HeapArray[3], is(1));
+        Assert.assertThat(heap.HeapArray[4], is(3));
+        Assert.assertThat(heap.HeapArray[5], is(2));
+        Assert.assertThat(heap.HeapArray[6], is(5));
+    }
+
+    @Test
+    public void getMaxDefaultTest() {
         int[] array = {12, 8, 4, 6, 2, 14, 10};
         Heap heap = new Heap();
         heap.MakeHeap(array, 2);
-
         Assert.assertThat(heap.HeapArray[0], is(14));
-        Assert.assertThat(heap.HeapArray[1], is(12));
-        Assert.assertThat(heap.HeapArray[2], is(10));
-        Assert.assertThat(heap.HeapArray[3], is(8));
-        Assert.assertThat(heap.HeapArray[4], is(6));
+        Assert.assertThat(heap.HeapArray[1], is(8));
+        Assert.assertThat(heap.HeapArray[2], is(12));
+        Assert.assertThat(heap.HeapArray[3], is(6));
+        Assert.assertThat(heap.HeapArray[4], is(2));
         Assert.assertThat(heap.HeapArray[5], is(4));
-        Assert.assertThat(heap.HeapArray[6], is(2));
-    }
-
-    @Test
-    public void getMaxDefTest() {
-
-        int[] array = {12, 8, 4, 6, 2, 14, 10};
-        Heap heap = new Heap();
-        heap.MakeHeap(array, 2);
-
-        Assert.assertThat(heap.HeapArray[0], is(14));
-        Assert.assertThat(heap.HeapArray[1], is(12));
-        Assert.assertThat(heap.HeapArray[2], is(10));
-        Assert.assertThat(heap.HeapArray[3], is(8));
-        Assert.assertThat(heap.HeapArray[4], is(6));
-        Assert.assertThat(heap.HeapArray[5], is(4));
-        Assert.assertThat(heap.HeapArray[6], is(2));
-
+        Assert.assertThat(heap.HeapArray[6], is(10));
         Assert.assertThat(heap.GetMax(), is(14));
         Assert.assertThat(heap.HeapArray[0], is(12));
-        Assert.assertThat(heap.HeapArray[1], is(10));
-        Assert.assertThat(heap.HeapArray[2], is(8));
-        Assert.assertThat(heap.HeapArray[3], is(6));
-        Assert.assertThat(heap.HeapArray[4], is(4));
-        Assert.assertThat(heap.HeapArray[5], is(2));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-        Assert.assertThat(heap.GetMax(), is(12));
-        Assert.assertThat(heap.HeapArray[0], is(10));
         Assert.assertThat(heap.HeapArray[1], is(8));
-        Assert.assertThat(heap.HeapArray[2], is(6));
-        Assert.assertThat(heap.HeapArray[3], is(4));
+        Assert.assertThat(heap.HeapArray[2], is(10));
+        Assert.assertThat(heap.HeapArray[3], is(6));
         Assert.assertThat(heap.HeapArray[4], is(2));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-        Assert.assertThat(heap.GetMax(), is(10));
-        Assert.assertThat(heap.HeapArray[0], is(8));
-        Assert.assertThat(heap.HeapArray[1], is(6));
-        Assert.assertThat(heap.HeapArray[2], is(4));
-        Assert.assertThat(heap.HeapArray[3], is(2));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-        Assert.assertThat(heap.GetMax(), is(8));
-        Assert.assertThat(heap.HeapArray[0], is(6));
-        Assert.assertThat(heap.HeapArray[1], is(4));
-        Assert.assertThat(heap.HeapArray[2], is(2));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-        Assert.assertThat(heap.GetMax(), is(6));
-        Assert.assertThat(heap.HeapArray[0], is(4));
-        Assert.assertThat(heap.HeapArray[1], is(2));
-        Assert.assertThat(heap.HeapArray[2], is(0));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-        Assert.assertThat(heap.GetMax(), is(4));
-        Assert.assertThat(heap.HeapArray[0], is(2));
-        Assert.assertThat(heap.HeapArray[1], is(0));
-        Assert.assertThat(heap.HeapArray[2], is(0));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-        Assert.assertThat(heap.GetMax(), is(2));
-        Assert.assertThat(heap.HeapArray[0], is(0));
-        Assert.assertThat(heap.HeapArray[1], is(0));
-        Assert.assertThat(heap.HeapArray[2], is(0));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
-        Assert.assertThat(heap.GetMax(), is(-1));
-        Assert.assertThat(heap.HeapArray[0], is(0));
-        Assert.assertThat(heap.HeapArray[1], is(0));
-        Assert.assertThat(heap.HeapArray[2], is(0));
-        Assert.assertThat(heap.HeapArray[3], is(0));
-        Assert.assertThat(heap.HeapArray[4], is(0));
-        Assert.assertThat(heap.HeapArray[5], is(0));
-        Assert.assertThat(heap.HeapArray[6], is(0));
-
+        Assert.assertThat(heap.HeapArray[5], is(4));
     }
 
     @Test
-    public void errorDefTest() {
-
-        int[] array = {110, 90, 40, 70, 80, 30, 10, 20, 50, 60, 65, 31, 29, 11, 9};
+    public void getMaxEveryStepTest() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
         Heap heap = new Heap();
-        heap.MakeHeap(array, 3);
+        heap.MakeHeap(array, 2);
+        Assert.assertThat(heap.GetMax(), is(7));
+        Assert.assertThat(heap.HeapArray[0], is(6));
+        Assert.assertThat(heap.HeapArray[1], is(4));
+        Assert.assertThat(heap.HeapArray[2], is(5));
+        Assert.assertThat(heap.HeapArray[3], is(1));
+        Assert.assertThat(heap.HeapArray[4], is(3));
+        Assert.assertThat(heap.HeapArray[5], is(2));
+    }
 
-        Assert.assertThat(heap.HeapArray[0], is(110));
-        Assert.assertThat(heap.HeapArray[1], is(90));
-        Assert.assertThat(heap.HeapArray[2], is(40));
-        Assert.assertThat(heap.HeapArray[3], is(70));
+    @Test
+    public void getMaxEveryStepReverseTest() {
+        int[] array = {7, 6, 5, 4, 3, 2, 1};
+        Heap heap = new Heap();
+        heap.MakeHeap(array, 2);
+        Assert.assertThat(heap.HeapArray[0], is(7));
+        Assert.assertThat(heap.HeapArray[1], is(6));
+        Assert.assertThat(heap.HeapArray[2], is(5));
+        Assert.assertThat(heap.HeapArray[3], is(4));
+        Assert.assertThat(heap.HeapArray[4], is(3));
+        Assert.assertThat(heap.HeapArray[5], is(2));
+        Assert.assertThat(heap.HeapArray[6], is(1));
+        Assert.assertThat(heap.GetMax(), is(7));
+        Assert.assertThat(heap.HeapArray[0], is(6));
+        Assert.assertThat(heap.HeapArray[1], is(4));
+        Assert.assertThat(heap.HeapArray[2], is(5));
+        Assert.assertThat(heap.HeapArray[3], is(1));
+        Assert.assertThat(heap.HeapArray[4], is(3));
+        Assert.assertThat(heap.HeapArray[5], is(2));
+    }
+
+    @Test
+    public void getMaxOverDepthStepTest() {
+        int[] array = {1, 1, 1, 1, 1, 1, 1};
+        Heap heap = new Heap();
+        heap.MakeHeap(array, 1);
+        Assert.assertThat(heap.HeapArray[0], is(1));
+        Assert.assertThat(heap.HeapArray[1], is(1));
+        Assert.assertThat(heap.HeapArray[2], is(1));
+        Assert.assertThat(heap.GetMax(), is(1));
+        Assert.assertThat(heap.HeapArray.length, is(2));
+        Assert.assertThat(heap.HeapArray[0], is(1));
+        Assert.assertThat(heap.HeapArray[1], is(1));
+    }
+
+    @Test
+    public void det() {
+
+        int[] array = {1, 2, 3, 4};
+        Heap heap = new Heap();
+        heap.MakeHeap(array, 2);
+
+        Assert.assertThat(heap.HeapArray[0], is(4));
+        Assert.assertThat(heap.HeapArray[1], is(3));
+        Assert.assertThat(heap.HeapArray[2], is(2));
+        Assert.assertThat(heap.HeapArray[3], is(1));
+
+
+        Assert.assertThat(heap.GetMax(), is(4));
+        Assert.assertThat(heap.HeapArray[0], is(3));
+        Assert.assertThat(heap.HeapArray[1], is(1));
+        Assert.assertThat(heap.HeapArray[2], is(2));
 
     }
+
+
 }
